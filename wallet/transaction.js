5660ab041dcb90f180b1312aa7acbc96eec4a150
samcms1234 <samdevpro99@gmail.com>
2022-10-01 01:52:58 +0530

    added static transaction function to generate outputs for sender and receipient

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -6,4 +6,22 @@ class Transaction {
         this.input = null;
         this.outputs = [];
     }
+
+    static newTransaction(senderWallet, receipient, amount) {
+        const transaction = new this();
+        
+        if(amount > senderWallet.balance) {
+            console.log(`Amount: ${amount} exceeds balance.`);
+            return;
+        }
+
+        transaction.outputs.push(...[
+            { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
+            { amount, address: receipient }
+        ])
+
+        return transaction;
+    }
 }
+
+module.exports = Transaction;
