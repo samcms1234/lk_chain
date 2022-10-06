4434364d2d5af6cf7172af2f4330de752c127ea5
samcms1234 <samdevpro99@gmail.com>
2022-10-06 23:30:32 +0530

    added functionality to fetch existing transactions and post a transaction to the TransactionPool

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -17,11 +21,20 @@ app.get('/blocks', (req, res) => {
 
 app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
-    console.log('New block added: '+ block.toString());
+    console.log(`New block added: ${block.toString()}`);
     p2pServer.syncChains();
 
     res.redirect('/blocks');
 });
 
-app.listen(HTTP_PORT, () => console.log('Listening on port '+HTTP_PORT));
+app.get('/transactions', (req, res) =>{
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { receipient, amount } = req.body;
+    const transaction = wallet.createTransaction(receipient, amount, tp);
+    res.redirect('/transactions');
+});
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
