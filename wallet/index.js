51c8c1eac9602db882e5943fc8c451e57836cada
samcms1234 <samdevpro99@gmail.com>
2022-10-23 23:55:41 +0530

    functionality to calculate balance of a wallet

--- wallet/index.js
+++ wallet/index.js
@@ -19,7 +19,9 @@ class Wallet {
         return this.keyPair.sign(dataHash);
     }
 
-    createTransaction(receipient, amount, transactionPool) {
+    createTransaction(receipient, amount, blockchain, transactionPool) {
+        this.balance = this.calculateBalance(blockchain);
+
         if (amount > this.balance) {
             console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
             return;
@@ -35,6 +37,38 @@ class Wallet {
         }
 
         return transaction;
+    }
+
+    calculateBalance(blockchain) {
+        let balance = this.balance;
+        let transactions = [];
+        blockchain.chain.forEach(block => block.data.forEach(transaction => {
+             transactions.push(transaction);
+        }));
+
+        let startTime = 0;
+
+        const walletInputTs = transactions
+            .filter(transaction => transaction.input.address === this.publicKey);
+        
+        if (walletInputTs.length > 0) {
+        const recentInputT = walletInputTs.reduce(
+            (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current
+        );
+
+        balance = recentInputT.outputs.find(Output => output.address === this.publicKey).amount;
+        startTime = recentInputT.input.timestamp;
+        }
+
+        transactions.forEach(transaction => {
+            if (transaction.input.timestamp > startTime) {
+                transaction.outputs.find(output => {
+                    if (output.address === this.publicKey) {
+                        balance += output.amount;
+                    }
+                });
+            }
+        })
     }
 
     static blockchainWallet() {
